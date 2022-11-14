4b6c71dd69c8bab38cfe8ff46e6cd8be5be7713f
joabe <joabe.18@hotmail.com>
2022-11-14 15:41:11 -0300

    Project created

---
+++ backend/src/test/java/com/joabejw/dsmeta/DsmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joabejw.dsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
