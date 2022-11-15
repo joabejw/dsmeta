b4e6ce4eedc3a0f93f0e2bf0df5044a6e0e5d528
joabe <joabe.18@hotmail.com>
2022-11-15 18:25:36 -0300

    Other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,23 @@
+import Header from "./components/Header"
+
 import NotificationButton from "./components/NotificationButton"
+
+import SalesCard from "./components/SalesCard"
+
 function App() {
- return (
-    <>
-    <h1>H1</h1>
-    <NotificationButton /> 
-    </>
- )
-  
+   return (
+      <>
+         <Header />
+         <main>
+            <section id="sales">
+               <div className="dsmeta-container">
+                  <SalesCard />
+               </div>
+            </section>
+         </main>
+      </>
+   )
+
 }
 
 export default App
